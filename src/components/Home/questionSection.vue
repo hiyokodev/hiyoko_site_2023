f1feea8a6bc60bb98d77852ddf0bcf5f55e31621
yume-saijo <my.dmf.125.@gmail.com>
2023-05-22 21:08:05 +0900

    Add:1つめの質問
    refs　#3
    1つめのQ&Aをかいたので試しにコミットします。

--- src/components/Home/questionSection.vue
+++ src/components/Home/questionSection.vue
@@ -3,9 +3,15 @@
     <div class="background"></div>
     <div class="contents">
       <h2>Question</h2>
+      <p>メンバーは何人くらい？</p>
+      <p>A.約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、ご友人と一緒に参加されるのもよいと思います。</p>
+      
     </div>
   </section>
 </template>
+
+
+
 <script>
 export default {}
 </script>
