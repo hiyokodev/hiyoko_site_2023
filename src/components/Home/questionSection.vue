48de1212f48385d54138a71827452d8279e1cdcd
yume-saijo <my.dmf.125.@gmail.com>
2023-05-22 21:56:51 +0900

    Add:cssで変更あり
    refs　#3
    cssでスタイルを整えました。

--- src/components/Home/questionSection.vue
+++ src/components/Home/questionSection.vue
@@ -3,21 +3,50 @@
     <div class="background"></div>
     <div class="contents">
       <h2>Question</h2>
+
       <div class="question">
-        <p>メンバーは何人くらい。</p>
-        <p>A.約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、ご友人と一緒に参加されるのもよいと思います。</p>
+        <div class="questionq">
+          <p>メンバーは何人くらい。</p>
+        </div>
+        <div class="anser">
+          <p>A.約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、ご友人と一緒に参加されるのもよいと思います。</p>
+        </div>
+      </div>
       
-        <p>参加するにあたり、何か必要なものはありますか。</p>
-        <p>A特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。</p>
-      
-        <p>入ったらまずどんなことをするのでしょうか。</p>
-        <p>スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築などのセットアップもサポートさせていただきます。その後は定例会やオフラインイベントにご自由にご参加ください。</p>
-  
-        <p>仕事が忙しくなった場合、活動に参加できるか不安です。</p>
-        <p>活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、後から確認することもできます。</p>
-  
-        <p>上記の活動以外のこともしてみたいのですが、できるのでしょうか。</p>
-        <p>やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。</p>
+      <div class="question">
+        <div class="questionq">
+          <p>参加するにあたり、何か必要なものはありますか。</p>
+        </div>
+        <div class="anser">
+          <p>A.特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。</p>
+        </div>
+      </div>
+
+      <div class="question">
+        <div class="questionq">
+          <p>入ったらまずどんなことをするのでしょうか。</p>
+        </div> 
+        <div class="anser">
+          <p>A.スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築などのセットアップもサポートさせていただきます。その後は定例会やオフラインイベントにご自由にご参加ください。</p>
+        </div>
+      </div>
+
+      <div class="question">
+        <div class="questionq">
+          <p>仕事が忙しくなった場合、活動に参加できるか不安です。</p>
+        </div>
+        <div class="anser">
+          <p>A.活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、後から確認することもできます。</p>
+        </div>
+      </div>
+
+      <div class="question">
+        <div class="questionq">
+          <p>上記の活動以外のこともしてみたいのですが、できるのでしょうか。</p>
+        </div>
+        <div class="anser">
+          <p>A.やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。</p>
+        </div>
       </div>
 
     </div>
@@ -38,16 +67,31 @@ section {
 .background {
   width: 100%;
   height: 100%;
-  background-color: lightskyblue;
+  background-color: F5F5F5;
   position: absolute;
   z-index: -2;
 }
 
 .contents {
   padding: 20px;
+  margin:100px;
 }
 
 h2 {
   text-align: center;
+  font-size: 50px;
+  font-family: Bitter;
 }
+
+.question{
+  font-size: 20px;
+  margin-bottom: 40px;
+  border-bottom: 2px solid black;
+  padding: 40px;
+}
+
+.questionq{
+  font-weight: bold;
+}
+
 </style>
