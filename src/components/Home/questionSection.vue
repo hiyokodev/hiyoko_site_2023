90a733c7437ea84fb08a2b35e3864b17940fd383
yume-saijo <my.dmf.125.@gmail.com>
2023-05-28 14:14:29 +0900

    Fix:コードの修正
    refs #3
    クラス名、色コード、文章のQとAを追加して修正をしました。

--- src/components/Home/questionSection.vue
+++ src/components/Home/questionSection.vue
@@ -4,40 +4,40 @@
     <div class="contents">
       <h2>Question</h2>
 
-      <div class="question">
-        <div class="questionq">
-          <p>メンバーは何人くらい。</p>
+      <div class="questions">
+        <div class="question">
+          <p>Q.メンバーは何人くらい。</p>
         </div>
-        <div class="anser">
+        <div class="answer">
           <p>A.約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、ご友人と一緒に参加されるのもよいと思います。</p>
         </div>
 
-        <div class="questionq">
-          <p>参加するにあたり、何か必要なものはありますか。</p>
+        <div class="question">
+          <p>Q.参加するにあたり、何か必要なものはありますか。</p>
         </div>
-        <div class="anser">
-          <p>A特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。</p>
-        </div>
-
-        <div class="questionq">
-          <p>入ったらまずどんなことをするのでしょうか。</p>
-        </div>
-        <div class="anser">
-          <p>スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築などのセットアップもサポートさせていただきます。<br>その後は定例会やオフラインイベントにご自由にご参加ください。</p>
+        <div class="answer">
+          <p>A.特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。</p>
         </div>
 
-        <div class="questionq">
-          <p>仕事が忙しくなった場合、活動に参加できるか不安です。</p>
+        <div class="question">
+          <p>Q.入ったらまずどんなことをするのでしょうか。</p>
         </div>
-        <div class="anser">
-          <p>活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、後から確認することもできます。</p>
+        <div class="answer">
+          <p>A.スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築などのセットアップもサポートさせていただきます。<br>その後は定例会やオフラインイベントにご自由にご参加ください。</p>
         </div>
 
-        <div class="questionq">
-          <p>上記の活動以外のこともしてみたいのですが、できるのでしょうか。</p>
+        <div class="question">
+          <p>Q.仕事が忙しくなった場合、活動に参加できるか不安です。</p>
         </div>
-        <div class="anser">
-          <p>やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。</p>
+        <div class="answer">
+          <p>A.活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、後から確認することもできます。</p>
+        </div>
+
+        <div class="question">
+          <p>Q.上記の活動以外のこともしてみたいのですが、できるのでしょうか。</p>
+        </div>
+        <div class="answer">
+          <p>A.やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。</p>
         </div>
       </div>
 
@@ -59,7 +59,7 @@ section {
 .background {
   width: 100%;
   height: 100%;
-  background-color: F5F5F5;
+  background-color: #F5F5F5;
   position: absolute;
   z-index: -2;
 }
@@ -78,12 +78,12 @@ h2 {
 =======
 }
 
-.questionq{
+.question{
   font-weight: bold;
   font-size: 20px;
 }
 
-.anser{
+.answer{
   margin-bottom: 50px;
   border-bottom: 1px solid black;
   padding-bottom:30px;
@@ -91,11 +91,11 @@ h2 {
 >>>>>>> 5c35edf9f789f789ed9683ca20f2949235e73156
 }
 
-.questionq{
+.question{
   font-weight: bold;
   font-size: 20px;
 }
-
+ｓ
 .anser{
   margin-bottom: 50px;
   border-bottom: 1px solid black;
