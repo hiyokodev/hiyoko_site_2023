ed62c764cdfd120bc0b897d2b7b2239d9cbcb9e5
yume-saijo <my.dmf.125.@gmail.com>
2023-05-28 13:44:45 +0900

    Add:ブランチを統一
    refs #3
    國部さんのPCで編集したブランチと私のPCのブランチを統一しました。

--- src/components/Home/questionSection.vue
+++ src/components/Home/questionSection.vue
@@ -9,46 +9,35 @@
           <p>メンバーは何人くらい。</p>
         </div>
         <div class="anser">
-          <p>A.約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、<br>
-            ご友人と一緒に参加されるのもよいと思います。</p>
+          <p>A.約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、ご友人と一緒に参加されるのもよいと思います。</p>
         </div>
-      </div>
-      
-      <div class="question">
+
         <div class="questionq">
           <p>参加するにあたり、何か必要なものはありますか。</p>
         </div>
         <div class="anser">
-          <p>A.特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。</p>
+          <p>A特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。</p>
         </div>
-      </div>
 
-      <div class="question">
         <div class="questionq">
           <p>入ったらまずどんなことをするのでしょうか。</p>
-        </div> 
-        <div class="anser">
-          <p>A.スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築のセットアップもサポートさせていただきます。<br>
-               その後は定例会やオフラインイベントにご自由にご参加ください。</p>
         </div>
-      </div>
+        <div class="anser">
+          <p>スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築などのセットアップもサポートさせていただきます。<br>その後は定例会やオフラインイベントにご自由にご参加ください。</p>
+        </div>
 
-      <div class="question">
         <div class="questionq">
           <p>仕事が忙しくなった場合、活動に参加できるか不安です。</p>
         </div>
         <div class="anser">
-          <p>A.活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、<br>
-            後から確認することもできます。</p>
+          <p>活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、後から確認することもできます。</p>
         </div>
-      </div>
 
-      <div class="question">
         <div class="questionq">
           <p>上記の活動以外のこともしてみたいのですが、できるのでしょうか。</p>
         </div>
         <div class="anser">
-          <p>A.やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。</p>
+          <p>やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。</p>
         </div>
       </div>
 
@@ -82,19 +71,20 @@ section {
 
 h2 {
   text-align: center;
-  font-size: 50px;
   font-family: Century;
-}
-
-.question{
-  font-size: 20px;
-  margin-bottom: 40px;
-  border-bottom: 2px solid black;
-  padding: 40px;
+  font-size: 60px;
+  text-shadow: 1px 2px 3px #808080;
 }
 
 .questionq{
   font-weight: bold;
+  font-size: 20px;
 }
 
+.anser{
+  margin-bottom: 50px;
+  border-bottom: 1px solid black;
+  padding-bottom:30px;
+  font-size:18px;
+}
 </style>
