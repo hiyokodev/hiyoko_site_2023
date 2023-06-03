871e369f6600792dd091bca857a1334a914c1241
yume-saijo <my.dmf.125.@gmail.com>
2023-06-03 12:36:08 +0900

    Add: activityセクションの要素であるボックスを配置
    refs #7
    actuvityセクションにコンテンツを3つ配置するためのボックスを配置しました。ボックスには文字と仮の写真をいれています。
    写真のアドレスがわからないです。
    共有のhttpサーバーなどをたてて持ってくるのでしょうか。

--- src/components/Home/activitySection.vue
+++ src/components/Home/activitySection.vue
@@ -3,7 +3,25 @@
     <div class="background"></div>
     <div class="contents">
       <h2>Activity</h2>
+
+      <div class="activity">
+        <div class="activity-contents">
+          <p>WEB site</p>
+          <img src="C:\Users\yume\OneDrive\画像\カメラ ロール\8726b32a1b75702b6e9b4637aa58f8df.jpg">
+        </div>
+        <div class="activity-contents">
+          <p>Automation tools</p>
+          <img src="C:\Users\yume\OneDrive\画像\カメラ ロール\8726b32a1b75702b6e9b4637aa58f8df.jpg">
+        </div>
+        <div class="activity-contents">
+          <p>Lightning talk</p>
+          <img src="C:\Users\yume\OneDrive\画像\カメラ ロール\8726b32a1b75702b6e9b4637aa58f8df.jpg">
+        </div>
+      </div>
+
     </div>
+
+
   </section>
 </template>
 <script>
@@ -24,7 +42,7 @@ section {
 }
 
 .contents {
-  padding: 20px;
+  padding: 20px; 
 }
 
 h2 {
@@ -32,4 +50,24 @@ h2 {
   font-size: 60px;
   font-family: Century;
 }
+
+.activity{
+  text-align: center; 
+}
+
+.activity-contents{
+  width: 300px;
+  padding: 10px 30px;
+  margin: 10px 30px;
+  background: rgba(230, 230, 230, 0.7);
+  border-bottom: solid 6px #c6cbd0;
+  border-radius: 9px;
+  display: inline-block; 
+}
+
+.activity-contents p{
+  font-family: Bitter;
+  font-weight: bold;
+}
+
 </style>
