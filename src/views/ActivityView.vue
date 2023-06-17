53dafe6bf24e4ea3df7e23d33a7580cad0ed0377
yume-saijo <my.dmf.125.@gmail.com>
2023-06-17 19:29:13 +0900

    Add:Activityページのフォーマットを作成（<template>と<script>）
    refs #11
    ブランチを発行し、htmlとcssのフォーマットを作成しました。
    h2のタイトルだけ表示してます。

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -2,6 +2,36 @@
 
 <template>
   <main>
-    <p>アクティビティページです</p>
+    <div class="contents">
+      <h2>Activity</h2>
+
+      <div class="Web site">
+       
+      </div>
+
+      <div class="Automation tools">
+      </div>
+
+      <div class="Lightning talk">
+      </div>
+
+    </div>
   </main>
 </template>
+
+
+
+
+<script>
+export default {}
+</script>
+<style scoped>
+
+h2 {
+  text-align: center;
+  font-family: Century;
+  font-size: 60px;
+  text-shadow: 1px 2px 3px #808080;
+}
+
+</style>
