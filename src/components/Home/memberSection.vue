b9c9e82a844092e4a7664e12b6f7daa3690a0c8c
mayube <mayube.0107@icloud.com>
2023-06-20 01:16:16 +0900

    Add: Memberセクション編集
    refs #8
    メンバーセクションに写真の表示ができました。
    写真の枚数少ないですが、また写真入れて調整します。
    動きスピードと共に写真の大きさ設定。

--- src/components/Home/memberSection.vue
+++ src/components/Home/memberSection.vue
@@ -4,6 +4,22 @@
     <div class="contents">
       <h2>Member</h2>
     </div>
+
+ 
+    <div class="scroll">
+    <ul class="scroll-list">
+        <li><img class="slide" src="@/assets/img/member1.jpg" alt=""></li>
+        <li><img class="slide" src="@/assets/img/member2.jpg" alt=""></li>
+        <li><img class="slide" src="@/assets/img/member3.jpg" alt=""></li>
+    </ul>
+    <ul class="scroll-list">
+        <li><img class="slide" src="@/assets/img/member1.jpg" alt=""></li>
+        <li><img class="slide" src="@/assets/img/member2.jpg" alt=""></li>
+        <li><img class="slide" src="@/assets/img/member3.jpg" alt=""></li>
+    </ul>
+    </div>
+ 
+
   </section>
 </template>
 <script>
@@ -11,13 +27,13 @@ export default {}
 </script>
 <style scoped>
 section {
-  height: 500px;
+  height: 600px;
   position: relative;
 }
 
 .background {
-  width: 100%;
-  height: 100%;
+  width: 200%;
+  height: 200%;
   background-color: lightgoldenrodyellow;
   position: absolute;
   z-index: -2;
@@ -25,9 +41,50 @@ section {
 
 .contents {
   padding: 20px;
+  font-family: Century;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 10px;
 }
 
 h2 {
   text-align: center;
 }
+
+@keyframes loop {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(-100%);
+    }
+}
+.slide{
+  width: 240px;
+  height: auto;
+}
+.scroll {
+    margin: 10px;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    height: max-content;
+    overflow-y: hidden;
+    border: 2px solid;
+    padding: 0px;
+    max-width: 1800px;   
+    color: lightgoldenrodyellow; 
+}
+.scroll-list {
+    display: flex;
+    align-items: center;
+    animation: loop infinite linear 30s both;
+    li {
+        min-width: 10px;
+    }
+  }
+
+
+
 </style>
