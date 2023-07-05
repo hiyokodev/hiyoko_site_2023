7158d8c83ee69007a0f8b7cf984dfcce9f28857f
SugarWars <sugarwarsworks@gmail.com>
2023-07-06 08:34:20 +0900

    Refactor: MemberViewのレイアウト調整
    refs #10
    - ブラウザ幅に対して要素サイズが適切に可変するように修正

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -4,17 +4,17 @@
   <main>
     <div class="contents">
       <h2>Member</h2>
+      <div class="btn">
+        <div class="photos">
+          <div><img src="@/assets/img/tauti_akane1.jpg" /></div>
+          <div><img src="@/assets/img/takasu_masayuki1.jpg" /></div>
+          <div><img src="@/assets/img/kunibe_mayu1.jpg" /></div>
+          <div><img src="@/assets/img/saijyo_yume1.jpg" /></div>
+          <div><img src="@/assets/img/koyama_wataru1.jpg" /></div>
+          <div><img src="@/assets/img/nomura_mao1.jpg" /></div>
+        </div>
+      </div>
     </div>
-    <div class="btn">
-    <ul class="photos">
-        <li><img src="@/assets/img/tauti_akane1.jpg"></li>
-        <li><img src="@/assets/img/takasu_masayuki1.jpg"></li>
-        <li><img src="@/assets/img/kunibe_mayu1.jpg"></li>
-        <li><img src="@/assets/img/saijyo_yume1.jpg"></li>
-        <li><img src="@/assets/img/koyama_wataru1.jpg"></li>
-        <li><img src="@/assets/img/nomura_mao1.jpg"></li>
-    </ul>
-	</div>
   </main>
 </template>
 
@@ -22,9 +22,11 @@
 export default {}
 </script>
 <style scoped>
-
-.contents{
+.contents {
   text-align: center;
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 20px;
 }
 
 h2 {
@@ -33,37 +35,41 @@ h2 {
   text-shadow: 1px 2px 3px #808080;
 }
 
-.photos{
-	display: flex;
-	flex-wrap:wrap;
+.photos {
+  display: flex;
+  flex-wrap: wrap;
   justify-content: center;
 }
-.photos li {
-	width: calc(100% / 5);/*画像を横に5つ並べる場合*/
-	padding:0px 40px ;/*画像の左右上下に余白を入れる場合*/
-	box-sizing:border-box;
-  list-style: none;/*リストの黒い●を無くす*/
+.photos div {
+  width: calc(100% / 5); /*画像を横に5つ並べる場合*/
+  padding: 0px 10px; /*画像の左右上下に余白を入れる場合*/
+  box-sizing: border-box;
+  list-style: none; /*リストの黒い●を無くす*/
 }
-.photos li img {
-	max-width:100%; /*画像のはみだしを防ぐ*/
+.photos div img {
+  max-width: 100%; /*画像のはみだしを防ぐ*/
   min-width: 130px; /*画像の最低幅を定義*/
-	height: auto; /*画像の縦横比を維持*/
-	border:solid 10px #e1e1e1; /*←画像を1pxのグレーの枠線で囲む指定の場合*/
+  height: auto; /*画像の縦横比を維持*/
   flex-wrap: wrap;
-  margin:40px 0px;
+  margin: 10px 0px;
   border-radius: 35px; /* ちょっとだけ角丸 */
-  
 }
 
-.btn{
+@media screen and (max-width: 959px) {
+  /* 959px以下に適用されるCSS（タブレット用） */
+  .photos div img {
+    min-width: 30px; /*画像の最低幅を定義*/
+    border-radius: 5px; /* ちょっとだけ角丸 */
+  }
+}
+
+.btn {
   .btn:hover {
-  -webkit-transform: scale(1.3);
-	transform: scale(1.3);
+    -webkit-transform: scale(1.3);
+    transform: scale(1.3);
+  }
+  .btn:hover img:nth-of-type(2) {
+    opacity: 0;
+  }
 }
-.btn:hover img:nth-of-type(2) {
-  opacity: 0;
-}
-}
-
-
 </style>
