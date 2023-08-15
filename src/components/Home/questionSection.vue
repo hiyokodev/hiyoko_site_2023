12657d8a4cca7da2767b8c00110445685af239f6
SugarWars <sugarwarsworks@gmail.com>
2023-08-15 22:27:43 +0900

    Add: Questionセクションの整備
    refs #15
    - Q・Aアイコンの追加
    - 質問・回答文をdataにまとめてv-forで整理

--- src/components/Home/questionSection.vue
+++ src/components/Home/questionSection.vue
@@ -4,50 +4,14 @@
     <div class="contents">
       <h2>Question</h2>
 
-      <div class="questions">
+      <div class="content" v-for="content in contents" :key="content.question">
         <div class="question">
-          <p>Q.メンバーは何人くらい。</p>
+          <div class="question_icon">Q</div>
+          <p class="question_text">{{ content.question }}</p>
         </div>
         <div class="answer">
-          <p>
-            A.約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、ご友人と一緒に参加されるのもよいと思います。
-          </p>
-        </div>
-
-        <div class="question">
-          <p>Q.参加するにあたり、何か必要なものはありますか。</p>
-        </div>
-        <div class="answer">
-          <p>
-            A.特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。
-          </p>
-        </div>
-
-        <div class="question">
-          <p>Q.入ったらまずどんなことをするのでしょうか。</p>
-        </div>
-        <div class="answer">
-          <p>
-            A.スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築などのセットアップもサポートさせていただきます。<br />その後は定例会やオフラインイベントにご自由にご参加ください。
-          </p>
-        </div>
-
-        <div class="question">
-          <p>Q.仕事が忙しくなった場合、活動に参加できるか不安です。</p>
-        </div>
-        <div class="answer">
-          <p>
-            A.活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、後から確認することもできます。
-          </p>
-        </div>
-
-        <div class="question">
-          <p>Q.上記の活動以外のこともしてみたいのですが、できるのでしょうか。</p>
-        </div>
-        <div class="answer">
-          <p>
-            A.やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。
-          </p>
+          <div class="answer_icon">A</div>
+          <p class="answer_text">{{ content.answer }}</p>
         </div>
       </div>
     </div>
@@ -55,7 +19,39 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      contents: [
+        {
+          question: 'メンバーは何人くらい。',
+          answer:
+            '約30名ほどで活動中で、男女ともに参加しやすい雰囲気です。お一人で飛び込むのが不安という方は、ご友人と一緒に参加されるのもよいと思います。'
+        },
+        {
+          question: '参加するにあたり、何か必要なものはありますか。',
+          answer:
+            '特にありません。活動に使用するツール（Slack・Notion・Zoom）がPCやスマートフォンから利用できれば大丈夫です。'
+        },
+        {
+          question: '入ったらまずどんなことをするのでしょうか。',
+          answer:
+            'スムーズに参加してもらうための各種説明会に加え、ツールの使い方や環境構築などのセットアップもサポートさせていただきます。その後は定例会やオフラインイベントにご自由にご参加ください。'
+        },
+        {
+          question: '仕事が忙しくなった場合、活動に参加できるか不安です。',
+          answer:
+            '活動はすべて任意参加なので、ご自身のスケジュール優先で大丈夫です。内容はアーカイブに残るので、後から確認することもできます。'
+        },
+        {
+          question: '上記の活動以外のこともしてみたいのですが、できるのでしょうか。',
+          answer:
+            'やってみたいことの提案は大歓迎です。ここに載せきれていない活動も多くありますし、アイデアは常に募集中です。'
+        }
+      ]
+    }
+  }
+}
 </script>
 <style scoped>
 section {
@@ -70,12 +66,6 @@ section {
   z-index: -2;
 }
 
-.contents {
-  padding: 20px;
-  margin: 100px;
-  font-family: 'Yomogi';
-}
-
 h2 {
   text-align: center;
   font-size: 60px;
@@ -83,15 +73,64 @@ h2 {
   text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2);
 }
 
+.contents {
+  padding: 20px;
+  margin: 100px;
+  font-family: 'Yomogi';
+}
+
+.content {
+  margin-bottom: 80px;
+}
+
 .question {
+  display: flex;
+  padding: 15px;
   font-weight: bold;
   font-size: 20px;
 }
 
+.question_icon {
+  display: block;
+  text-align: center;
+  color: #fff;
+  background: rgb(160, 160, 160);
+  width: 32px;
+  height: 32px;
+  line-height: 32px;
+  border-radius: 50%;
+  box-shadow: 1px 1px 2px rgb(195, 195, 195);
+}
+
+.question_text {
+  margin: 0;
+  width: calc(100% - 32px);
+  padding-left: 15px;
+  line-height: 32px;
+}
+
 .answer {
-  margin-bottom: 50px;
-  border-bottom: 1px solid black;
-  padding-bottom: 30px;
+  display: flex;
+  padding: 15px;
   font-size: 18px;
+}
+
+.answer_icon {
+  display: block;
+  text-align: center;
+  color: rgb(100, 100, 100);
+  background: rgb(251, 234, 146);
+  width: 32px;
+  height: 32px;
+  line-height: 32px;
+  border-radius: 50%;
+  box-shadow: 1px 1px 2px rgb(195, 195, 195);
+}
+
+.answer_text {
+  margin: 0;
+  width: calc(100% - 32px);
+  padding-left: 15px;
+  line-height: 32px;
 }
 </style>
