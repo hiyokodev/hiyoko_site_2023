becc1a1cf41930e4ec226e1954d249e363ed21b5
yume-saijo <my.dmf.125.@gmail.com>
2023-08-17 21:04:46 +0900

    Add:写真を挿入しました。
    refs #11
    最初からにしてます。写真をいれました。フォントを呼び出してみました。

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -2,112 +2,62 @@
 
 <template>
   <main>
-    <div class="contents">
+    <div class="top">
       <h2>Activity</h2>
+    </div>
 
-      <div class="Website">
-        <div class="daen one"></div>
-        <div class="tyouhoukei tone">
-          <h3>Web Site</h3>
-          <table border="0">
-            <tr>
-              <td><img src="@/assets/img/web1.jpeg" class="img"></td>
-              <td><img src="@/assets/img/web2.jpeg" class="img"></td>
-              <td><img src="@/assets/img/web3.jpg" class="img"></td>
-            </tr>
-          </table>
-          <p>webデザインからプログラミングまで一貫して制作します。経験者、初心者にかかわらず、チームで
-             プロジェクト管理をしてすすめていきます。キニナルを形にしてみませんか。
-             <br>
-             <br>私達のチームは基本オフラインでMTGを行います。
-             現在は「ひよこサイト」作成・運用・新プロジェクトも計画中です！
-             <br>○デスクでお菓子を囲んでワイワイ計画
-             <br>○ガリガリ作業
-             <br>○必要な素材の撮影</p>
-        </div>
-
+    <div class="contents">
+      <div class="web_site">
+        <img src="@/assets/img/website.jpg" class="web_img" />
       </div>
 
-      <div class="Automationtools">
-        <div class="daen two"></div>
-        <div class="tyouhoukei ttwo">
-          <h3>Automation tools</h3>
-          <table border="0">
-            <tr>
-              <td><img src="@/assets/img/auto.jpeg" class="img"></td>
-              <td><img src="@/assets/img/auto2.jpeg" class="img"></td>
-            </tr>
-          </table>
-          <p> ExcelのVBAを用いて、自動化の開発を行っています。
-              <br>マクロを組んだことがない人でも簡単に応用ができます。
-              <br>さまざまな自動化に今後も取り組み続けます。</p>
-        </div>
+      <div class="lightning_talk">
+        <img src="@/assets/img/LT.jpg" class="LT_img" />
       </div>
 
-      <div class="Lightning talk">
-        <div class="daen three"></div>
-        <div class="tyouhoukei tthree">
-          <h3>Lightning talk</h3>
-          <table border="0">
-            <tr>
-              <td><img src="@/assets/img/LT.jpg" class="img"></td>
-              <td><img src="@/assets/img/LT2.jpeg" class="img"></td>
-            </tr>
-          </table>
-          <p>AWSチームでやっているプレゼンコーナーのようなものを、
-             <br>テーマを拡張して10分程で定例会で発表します。
-             <br>〇〇の機器の✗✗の設定の仕方、Linuxコマンド、環境変数とパスについてなど、
-             <br>各自が好きなテーマで解説をし、メンバーと共有をして様々な知見を手に入れることができます。
-             <br>
-             <br>今後はさらに大きなプロジェクト単位でのグループプレゼンテーション等を考えています。
-             <br>過去の資料はこちらからみることができます。</p>
-        </div>
+      <div class="events">
+        <img src="@/assets/img/events.jpg" class="events_img" />
       </div>
-
     </div>
   </main>
 </template>
-
-
-
 
 <script>
 export default {}
 </script>
 <style scoped>
-
-.contents{
+.contents {
   text-align: center;
 }
 
 h2 {
-  font-family: Century;
+  font-family: 'Memelon';
   font-size: 60px;
   text-shadow: 1px 2px 3px #808080;
+  text-align: center;
 }
 
-.daen{
-  background-color:#ffec50;
+.daen {
+  background-color: #ffec50;
   border-radius: 50%;
-  margin:5% 30% 20% 10%;
+  margin: 5% 30% 20% 10%;
   width: 1000px;
   height: 700px;
-
 }
 
-.one{
+.one {
   position: absolute;
 }
 
-.two{
+.two {
   position: absolute;
 }
 
-.three{
+.three {
   position: absolute;
 }
 
-.tyouhoukei{
+.tyouhoukei {
   width: 900px;
   height: 600px;
   background-color: rgb(255, 255, 255);
@@ -116,55 +66,69 @@ h2 {
   padding-top: 5px;
 }
 
-.tone{
-  position:relative ;
+.tone {
+  position: relative;
   top: 150px;
   left: 450px;
 }
 
-.ttwo{
-  position:relative ;
+.ttwo {
+  position: relative;
   top: 150px;
   left: 450px;
 }
 
-.tthree{
-  position:relative ;
+.tthree {
+  position: relative;
   top: 150px;
   left: 450px;
 }
 
-
-
-h3{
+h3 {
   font-family: Century;
   font-size: 40px;
   text-align: left;
   margin-left: 70px;
 }
 
-.img{
-  width: 250px;
-  height: 250px;
+.web_img {
+  width: 800px;
+  height: 700px;
   opacity: 0.9;
+  margin: 10% 30% 10% 35%;
+  border-radius: 8%;
 }
 
-table{
+.LT_img {
+  width: 700px;
+  height: 900px;
+  opacity: 0.9;
+  margin: 10% 30% 25% 10%;
+  border-radius: 8%;
+}
+
+.events_img {
+  width: 950px;
+  height: 600px;
+  opacity: 0.9;
+  margin: 10% 30% 10% 30%;
+  border-radius: 8%;
+}
+
+table {
   margin: 0 auto;
 }
 
-.tyouhoukei p{
-  margin:30px 80px;
+.tyouhoukei p {
+  margin: 30px 80px;
   text-align: left;
 }
 
-
-.Website{
+.Website {
   padding-bottom: 300px;
 }
 
-.Automationtools{
+.Automationtools {
   padding-bottom: 300px;
 }
-
 </style>
