a4d2326289ded8f2b64358da04009d9003cf2b26
SugarWars <sugarwarsworks@gmail.com>
2023-08-20 21:32:47 +0900

    Add: Headerの作成
    refs #18
    - Headerのデザイン変更

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <header>
-    <nav v-for="Link in Links" :key="Link.name">
-      <RouterLink class="Link" :to="Link.path">{{ Link.name }}</RouterLink>
-    </nav>
+    <div class="Nav_Container">
+      <nav v-for="Link in Links" :key="Link.name">
+        <RouterLink class="Link" :to="Link.path">{{ Link.name }}</RouterLink>
+      </nav>
+    </div>
   </header>
 </template>
 <script>
@@ -29,14 +31,27 @@ export default {
 }
 </script>
 <style scoped>
+template {
+  position: absolute;
+}
 header {
+  position: relative;
   display: flex;
   justify-content: end;
   padding: 30px;
-  background-color: antiquewhite;
+}
+
+.Nav_Container {
+  position: absolute;
+  display: flex;
 }
 
 .Link {
-  margin: 0 10px;
+  margin: 0 15px;
+  color: #000;
+  font-size: 22px;
+  font-family: 'Mamelon';
+  text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
 }
 </style>
