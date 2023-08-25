c75759de9bce3b6271db4008d018ac5a3e180cb3
SugarWars <sugarwarsworks@gmail.com>
2023-08-25 22:46:56 +0900

    Add: scheduleSectionの作成
    refs #19
    - カレンダー画像を配置

--- src/components/Home/scheduleSection.vue
+++ src/components/Home/scheduleSection.vue
@@ -1,5 +1,12 @@
 <template>
-  <sectionLayout title="Schedule"> </sectionLayout>
+  <sectionLayout title="Schedule">
+    <p class="description">
+      1ヶ月の活動内容のイメージです。<br />どの集まりも任意参加なので、興味があるものだけ参加でOK！
+    </p>
+    <div class="Image_Container">
+      <img src="@/assets/img/Home/calender.png" />
+    </div>
+  </sectionLayout>
 </template>
 <script>
 import SectionLayout from './SectionLayout.vue'
@@ -9,4 +16,17 @@ export default {
   }
 }
 </script>
-<style scoped></style>
+<style scoped>
+.description {
+  margin: 50px;
+  line-height: 40px;
+}
+.Image_Container {
+  margin: 0 auto;
+  max-width: 800px;
+}
+
+img {
+  width: 100%;
+}
+</style>
