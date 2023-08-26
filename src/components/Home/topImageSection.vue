8957b43b462e4acdfcccca464fe27a363e6913dc
SugarWars <sugarwarsworks@gmail.com>
2023-08-26 11:18:19 +0900

    Fix: トップ画像の高さを修正
    refs #19
    - 100vhで縦幅いっぱいにせず、下の項目が少し見えるような高さに変更

--- src/components/Home/topImageSection.vue
+++ src/components/Home/topImageSection.vue
@@ -9,12 +9,17 @@
 export default {}
 </script>
 <style scoped>
+/* TODO: ヘッダーと合わせて背景色を変更（透過版の画像待ち） */
+section {
+  /* padding-bottom: 50px; */
+  background-color: #ffffe5;
+}
 .Img_Container {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100vh;
+  /* width: 100%; */
+  height: 100%;
 }
 
 img {
