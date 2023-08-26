528df676c2917049387c986e652d848b892582b7
yume-saijo <my.dmf.125.@gmail.com>
2023-08-26 14:37:42 +0900

    Add:要素を挿入
    refs #11
    ひよこ、足跡、ボタンを挿入しました。

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -14,6 +14,8 @@
         <img src="@/assets/img/website_egg.png" class="website_egg" />
         <img src="@/assets/img/website.jpg" class="website_img" />
         <div class="website_whitebox"></div>
+        <img src="@/assets/img/hiyoko_ashiato01.png" class="website_ashiato_img" />
+        <img src="@/assets/img/hiyoko_01.png" class="website_hiyoko_img" />
       </div>
 
       <!-- LT -->
@@ -21,6 +23,7 @@
         <img src="@/assets/img/LT_egg.png" class="LT_egg" />
         <img src="@/assets/img/LT.jpg" class="LT_img" />
         <div class="LT_whitebox"></div>
+        <img src="@/assets/img/hiyoko_ashiato02.png" class="LT_ashiato_img" />
       </div>
 
       <!-- Events -->
@@ -28,7 +31,15 @@
         <img src="@/assets/img/events_egg.png" class="events_egg" />
         <img src="@/assets/img/events.jpg" class="events_img" />
         <div class="events_whitebox"></div>
+        <img src="@/assets/img/hiyoko_02.png" class="events_hiyoko_img" />
+        <img src="@/assets/img/hiyoko_ashiato02.png" class="events_ashiato_img" />
       </div>
+
+      <div class="button">
+        <a class="btn">入会はこちら</a>
+      </div>
+
+      <img src="@/assets/img/hiyoko_3_01.png" class="hiyoko_img" />
     </div>
   </main>
 </template>
@@ -40,6 +51,7 @@ export default {}
 main {
   padding: 20px;
 }
+
 h2 {
   font-family: 'Memelon';
   font-size: 60px;
@@ -47,12 +59,6 @@ h2 {
   text-align: center;
 }
 
-h3 {
-  font-family: Century;
-  font-size: 40px;
-  text-align: left;
-  margin-left: 70px;
-}
 .container {
   position: relative;
   height: 1000px;
@@ -84,6 +90,21 @@ h3 {
   border-radius: 15px;
   padding-top: 5px;
 }
+
+.website_ashiato_img {
+  position: absolute;
+  top: 900px;
+  left: 600px;
+  height: 40px;
+}
+
+.website_hiyoko_img {
+  position: absolute;
+  top: 850px;
+  left: 1250px;
+  width: 80px;
+  height: 90px;
+}
 .LT_egg {
   position: absolute;
   top: 200px;
@@ -111,16 +132,24 @@ h3 {
   border-radius: 15px;
   padding-top: 5px;
 }
+
+.LT_ashiato_img {
+  position: absolute;
+  top: 1000px;
+  left: 150px;
+  height: 40px;
+  transform: skew(-10deg, 5deg);
+}
 .events_egg {
   position: absolute;
-  top: 100px;
+  top: 150px;
   left: 200px;
   width: 900px;
   text-shadow: 1px 2px 3px #808080;
 }
 .events_img {
   position: absolute;
-  top: 100px;
+  top: 150px;
   left: 350px;
   width: 800px;
   opacity: 0.9;
@@ -130,7 +159,7 @@ h3 {
 
 .events_whitebox {
   position: absolute;
-  top: 340px;
+  top: 390px;
   left: 150px;
   width: 550px;
   height: 400px;
@@ -139,6 +168,51 @@ h3 {
   border-radius: 15px;
   padding-top: 5px;
 }
+
+.events_ashiato_img {
+  position: absolute;
+  top: 900px;
+  left: 700px;
+  height: 40px;
+  transform: skew(10deg, -5deg);
+}
+.events_hiyoko_img {
+  position: absolute;
+  top: 900px;
+  left: 600px;
+  width: 80px;
+  height: 90px;
+}
+
+.button {
+  text-align: center;
+}
+.btn {
+  padding: 8px 24px;
+  margin-top: 100px;
+  display: inline-block;
+  background: #e5e5e2;
+  border-bottom: solid 4px #bdbdba;
+  font-size: 30px;
+  font-family: 'Memelon';
+  border-radius: 20px;
+  letter-spacing: 1px;
+  cursor: pointer;
+}
+
+.btn:active {
+  border-bottom: none;
+  position: relative;
+  top: 4px;
+}
+
+.hiyoko_img {
+  width: 400px;
+  height: 150px;
+  float: right;
+  margin-top: 150px;
+}
+
 /* .back_egg2 {
   margin-top: 1000px;
   margin-left: 800px;
