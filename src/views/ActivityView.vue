647c4cb4c9a88c5e727646f6a322dd47ffe09f6c
SugarWars <sugarwarsworks@gmail.com>
2023-08-27 22:07:33 +0900

    Add: Activityページに背景の卵を実装（途中）
    refs #22
    - PC・タブレットビューは仮画像が背景透過されてしないため調整が難しい
    - スマートフォンビューのみ調整済

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,6 +1,7 @@
 <template>
   <Header />
   <main>
+    <BackgroundEggs :eggsInfo="eggsInfo" />
     <h2 class="top">Activity</h2>
     <div class="contents">
       <Website />
@@ -13,17 +14,46 @@
 
 <script>
 import Header from '../components/common/Header.vue'
+import Button from '../components/common/Button.vue'
+import BackgroundEggs from '../components/common/BackgroundEggs.vue'
 import Website from '../components/Activity/Website.vue'
 import LT from '../components/Activity/LT.vue'
 import Events from '../components/Activity/Events.vue'
-import Button from '../components/common/Button.vue'
 export default {
   components: {
     Header,
+    Button,
+    BackgroundEggs,
     Website,
     LT,
-    Events,
-    Button
+    Events
+  },
+  data() {
+    return {
+      eggsInfo: {
+        pc: [
+          { top: 100, left: 5 },
+          { top: 400, left: 85 },
+          { top: 700, left: 25 },
+          { top: 1000, left: 70 },
+          { top: 1300, left: 10 }
+        ],
+        tb: [
+          { top: 100, left: 5 },
+          { top: 400, left: 85 },
+          { top: 600, left: 15 },
+          { top: 1000, left: 70 },
+          { top: 1300, left: 10 }
+        ],
+        sp: [
+          { top: 100, left: 75 },
+          { top: 300, left: 5 },
+          { top: 600, left: 55 },
+          { top: 1000, left: 10 },
+          { top: 1300, left: 80 }
+        ]
+      }
+    }
   }
 }
 </script>
