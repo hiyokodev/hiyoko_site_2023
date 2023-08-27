91dd1c006003cf3b4fb423d68bfd9b9e02e3d535
SugarWars <sugarwarsworks@gmail.com>
2023-08-27 11:43:47 +0900

    Add: Activityページを仮画像に差し替え
    refs #11
    - 3点セット画像で表示するテスト
    - コンポーネントをViewに集約

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -3,26 +3,46 @@
   <main>
     <h2 class="top">Activity</h2>
     <div class="contents">
-      <Website />
-      <LT />
-      <Events />
-      <Button message="入会はこちら！" url="/" />
+      <!-- WebSite -->
+      <div class="container">
+        <div class="img_container">
+          <img src="@/assets/img/Activity/tmp_website.png" class="content_img" />
+        </div>
+        <div class="hiyoko_container">
+          <img src="@/assets/img/hiyoko_ashiato01.png" class="website_ashiato" />
+          <img src="@/assets/img/hiyoko_01.png" class="website_hiyoko" />
+        </div>
+      </div>
+      <!-- LT -->
+      <div class="container">
+        <div class="img_container">
+          <img src="@/assets/img/Activity/tmp_LT.png" class="content_img" />
+        </div>
+        <div class="hiyoko_container">
+          <img src="@/assets/img/hiyoko_ashiato02.png" class="LT_ashiato" />
+        </div>
+      </div>
+      <!-- Events -->
+      <div class="container">
+        <div class="img_container">
+          <img src="@/assets/img/Activity/tmp_events.png" class="content_img" />
+        </div>
+        <div class="hiyoko_container">
+          <img src="@/assets/img/hiyoko_02.png" class="events_hiyoko" />
+          <img src="@/assets/img/hiyoko_ashiato02.png" class="events_ashiato" />
+        </div>
+      </div>
+      <Button message="入会はこちら！" url="#" />
     </div>
   </main>
 </template>
 
 <script>
 import Header from '../components/common/Header.vue'
-import Website from '../components/Activity/Website.vue'
-import LT from '../components/Activity/LT.vue'
-import Events from '../components/Activity/Events.vue'
 import Button from '../components/common/Button.vue'
 export default {
   components: {
     Header,
-    Website,
-    LT,
-    Events,
     Button
   }
 }
@@ -31,7 +51,6 @@ export default {
 main {
   padding: 20px;
 }
-
 .top {
   text-shadow: 1px 2px 3px #808080;
   text-align: center;
@@ -39,16 +58,48 @@ main {
   font-size: 500%;
 }
 
-h2 {
-  font-family: 'Memelon';
-  font-size: 50px;
+.contents {
+  max-width: 1500px;
 }
 
-p {
-  padding-left: 7%;
-  padding-right: 7%;
-  font-size: 18px;
-  letter-spacing: 1.2px;
-  font-family: 'Yomogi';
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto 100px auto;
+}
+.img_container {
+  margin: 0 auto;
+  min-width: 800px;
+}
+.content_img {
+  width: 100%;
+}
+.hiyoko_container {
+  display: flex;
+  align-items: flex-end;
+  margin: 0 auto;
+}
+.website_ashiato {
+  margin-right: 20px;
+  height: 30px;
+}
+
+.website_hiyoko {
+  margin-top: 30px;
+  height: 100px;
+}
+
+.LT_ashiato {
+  margin-top: 100px;
+  height: 30px;
+}
+.events_hiyoko {
+  margin-top: 30px;
+  margin-right: 20px;
+  height: 100px;
+}
+.events_ashiato {
+  height: 30px;
 }
 </style>
